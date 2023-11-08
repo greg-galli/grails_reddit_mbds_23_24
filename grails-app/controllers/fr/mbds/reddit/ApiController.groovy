10f8ddb053b682fcda475978ddd0304924fca44b
Gregory Galli <greg.galli@gmail.com>
2023-11-08 10:39:06 +0100

    Added base API

---
+++ grails-app/controllers/fr/mbds/reddit/ApiController.groovy
@@ -0,0 +1,83 @@
+package fr.mbds.reddit
+
+import grails.converters.JSON
+import grails.converters.XML
+
+class ApiController {
+
+    def userService
+
+    /**
+     * Ressource SINGLETON USER
+     * Répondra à /api/user sur tous les verbes utiles
+     * Methodes : GET / PUT / PATCH / DELETE
+     */
+    def user() {
+        switch (request.getMethod()) {
+            case "GET":
+                if (!params.id)
+                    return response.status = 400
+                def userInstance = userService.get(params.id)
+                if (!userInstance)
+                    return response.status = 404
+                renderThis(userInstance, request.getHeader("Accept"))
+                break
+            case "PUT":
+                break
+            case "PATCH":
+                break
+            case "DELETE":
+                break
+            default:
+                return response.status = 405
+                break
+        }
+        return response.status = 406
+    }
+
+    /**
+     * Ressource Collection USER
+     * Gestion de GET / POST
+     */
+    def users() {
+        switch (request.getMethod()) {
+            case "POST":
+                if (!request.getHeader("Content-Type").contains("json"))
+                    render(status: 400, text: "Le body doit être formaté en JSON")
+                def userInstance = new User(request.getJSON())
+                if (userInstance.validate())
+                {
+                    userService.save(userInstance)
+                    return response.status = 201
+                }
+                render(status: 400, text: "Une erreur est survenue pendant la sauvegarde de l'utilisateur")
+                break
+            case "GET":
+                break
+            default:
+                return response.status = 405
+                break
+        }
+        return response.status = 406
+
+    }
+
+    def renderThis(Object object, String accept) {
+        switch (accept) {
+            case "json":
+            case "application/json":
+            case "text/json":
+                render object as JSON
+                break
+            case "xml":
+            case "application/xml":
+            case "text/xml":
+                render object as XML
+                break
+            default:
+                render(status: 400, text: "L'encodage demandé n'est pas supporté")
+                break
+        }
+
+    }
+}
